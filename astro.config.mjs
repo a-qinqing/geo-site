d8fc29dbc6b5f3aee253265a78e4489f2232b0c8
andsgu <liuqi130434@gmail.com>
2026-08-07 22:24:23 +0800

    Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://geova.cn/",
+  site: "https://www.geova.cn/",
   redirects: {
     "/sitemap.xml": "/sitemap-index.xml",
   },
